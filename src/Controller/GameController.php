ce995360227ec395d415f81b0ea9c1ad2f3badda
Antoine3012 <77623896+Antoine3012@users.noreply.github.com>
2023-06-23 08:26:03 +0200

    modif

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -23,10 +23,16 @@ class GameController extends AbstractController
         $partie = new Partie();
         $partie->setGrille($grille);
 
-        // Créer deux utilisateurs pour la partie
-        $user1 = $this->getUser();
-        $user2 = new User(); // Remplacez par la façon dont vous créez un nouvel utilisateur
+        // Récupérer les utilisateurs joueurs de la partie
+        $user1 = $partie->getPlayer1(); // Utilisateur assigné à la partie (Joueur 1)
+        $user2 = $partie->getPlayer2(); // Utilisateur assigné à la partie (Joueur 2)
 
+        // Vérifier si les deux joueurs sont présents
+        // if (!$user1 || !$user2) {
+        //     throw new \Exception("Joueurs non disponibles");
+        // }
+
+        // Assigner les joueurs à la partie
         $partie->setPlayer1($user1);
         $partie->setPlayer2($user2);
 
@@ -43,8 +49,12 @@ class GameController extends AbstractController
         // Vérifier si la grille est pleine
         $isGrillePleine = $partie->is_full();
 
+        // Placez les pions sur la grille
+        foreach ($partie->getPions() as $pion) {
+            $grille->placerPion($pion->getPosVer(), $pion->getPosHor(), $pion);
+        }
+
         return $this->render('game/index.html.twig', [
-            'controller_name' => 'GameController',
             'partie' => $partie,
             'grille' => $grille,
             'isGrillePleine' => $isGrillePleine,
